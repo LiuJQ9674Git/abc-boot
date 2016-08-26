5f66ba7003a1eb58480ab51a863b67387ff851c2
U-hp\admin <admin@hp.(none)>
2016-08-27 04:36:14 +0800

    react react-bootstrap client

---
+++ react-bootstrap-getting-started/src/App.js
@@ -0,0 +1,26 @@
+
+
+import React from 'react';
+import ReactDom from 'react-dom';
+import 'bootstrap/less/bootstrap.less';
+import './styles/custom-styles.css';
+
+import Body from './Body';
+import Footer from './Footer';
+
+global.React = React;
+
+ReactDom.render(
+  <div>
+
+    <Body />
+    <Footer />
+  </div>
+  , document.getElementById('app'));
+
+/**
+ require( ['./activeButton'], function( activeButton){
+
+    console.log("test");
+});
+**/
---
+++ webpack/docs.config.js
@@ -0,0 +1,57 @@
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+import ip from 'ip';
+import path from 'path';
+import webpack from 'webpack';
+
+import baseConfig, { options, jsLoader } from './base.config';
+
+const webpackDevServerAddress = `http://${ip.address()}:${options.port}`;
+const cssSourceMap = options.debug ? '?sourceMap' : '';
+const reactHot = options.debug ? 'react-hot!' : '';
+
+const entryFile = './react-bootstrap-getting-started/src/App.js';
+const devEntryBundle = [
+  'webpack/hot/dev-server',
+  `webpack-dev-server/client?${webpackDevServerAddress}`,
+  entryFile,
+];
+
+baseConfig.plugins.push(new ExtractTextPlugin('[name].css'));
+if (options.debug) {
+  baseConfig.plugins.push(new webpack.NoErrorsPlugin());
+}
+
+export default {
+  ...baseConfig,
+
+  devtool: options.debug ? 'source-map' : null,
+  //entry: './react-bootstrap-getting-started/src/App.js',
+  //entry: './public/assets/js/demo/comments/entry.js',
+
+
+  entry: {
+    bundle: options.debug ? devEntryBundle : entryFile,
+  },
+
+
+  output: {
+    filename: '[name].js',
+    path: './build',
+    //publicPath: options.debug ? `${webpackDevServerAddress}/build/` : '/build/',
+    publicPath: '/build/'
+  },
+
+  module: {
+    loaders: [
+      { test: /\.js/, loader: `${reactHot}${jsLoader}`, exclude: /node_modules|Samples\.js/ },
+      { test: /Samples.js/, loader: `${reactHot}transform?brfs!${jsLoader}` },
+      { test: /\.css/,
+        loader: ExtractTextPlugin.extract('style', `css${cssSourceMap}`) },
+      { test: /\.less$/,
+        loader: ExtractTextPlugin.extract('style', `css${cssSourceMap}!less${cssSourceMap}`) },
+      { test: /\.json$/, loader: 'json' },
+      { test: /\.jpe?g$|\.gif$|\.png|\.ico$/, loader: 'file?name=[name].[ext]' },
+      { test: /\.eot$|\.ttf$|\.svg$|\.woff2?$/, loader: 'file?name=[name].[ext]' },
+    ],
+  },
+};
